b180002fee395601873f77a70bae02251da117ad
Midhun A Darvin <midhunadarvin@gmail.com>
2023-12-29 17:10:27 +0530

    fix: libuv pipeline buffer issue

--- src/CHttpParser.cpp
+++ src/CHttpParser.cpp
@@ -1,17 +1,14 @@
 
 #include <map>
 #include "CHttpParser.h"
-#include "Utils.h"
 
-using namespace std;
-
-void writeStringToBuffer(char *buffer, const char *str, size_t &position);
+void writestringToBuffer(char *buffer, const char *str, size_t &position);
 
 CHttpParser::CHttpParser() {}
 
 HttpMetadata CHttpParser::construct(void *buffer, int len)
 {
-    std::cout << "Constructing Http metadata (start): " << endl;
+    std::cout << "Constructing Http metadata (start): " << std::endl;
     HttpMetadata result;
     LineGrabber lineGrabber((char *)buffer, len);
 
@@ -35,24 +32,24 @@ HttpMetadata CHttpParser::construct(void *buffer, int len)
         {
             break;
         }
-        pair<std::string, std::string> temp = Utils::ChopLine(test);
+        std::pair<std::string, std::string> temp = Utils::ChopLine(test);
         result.headers.insert(temp);
     }
 
     // Rest of the content will be the body of the packet
-    string body = lineGrabber.getContentTillEof();
+    std::string body = lineGrabber.getContentTillEof();
     result.body = body;
-    std::cout << "Constructing Http metadata (end): " << endl;
+    std::cout << "Constructing Http metadata (end): " << std::endl;
     return result;
 }
 
 void *CHttpParser::deconstruct(HttpMetadata *httpMetadata)
 {
-    std::cout << "Deconstructing Http metadata (start): " << endl;
-    string buffer = "";
+    std::cout << "Deconstructing Http metadata (start): " << std::endl;
+    std::string buffer = "";
     buffer += (*httpMetadata).method + " " + (*httpMetadata).url + " " + (*httpMetadata).protocol + "\r\n";
 
-    map<string, string> *headers = &((*httpMetadata).headers);
+    std::map<std::string, std::string> *headers = &((*httpMetadata).headers);
     for (auto i : *headers)
     {
         buffer += i.first + ":" + i.second + "\r\n";
@@ -65,24 +62,24 @@ void *CHttpParser::deconstruct(HttpMetadata *httpMetadata)
     char *result = (char *)malloc(buffer_length);
     memcpy(result, buffer.c_str(), buffer_length);
 
-    std::cout << "Deconstructing Http metadata (end): " << endl;
+    std::cout << "Deconstructing Http metadata (end): " << std::endl;
     return result;
 }
 
 void CHttpParser::logMetadata(HttpMetadata *metadata)
 {
     // Log the Content and Forward the Data to the EndPoint
-    cout << "=============== CH HTTP ( packet contents ) ===================" << endl;
-    cout << "Method: " << metadata->method << endl;
-    cout << "URL: " << metadata->url << endl;
-    cout << "Protocol: " << metadata->protocol << endl;
+    std::cout << "=============== CH HTTP ( packet contents ) ===================" << std::endl;
+    std::cout << "Method: " << metadata->method << std::endl;
+    std::cout << "URL: " << metadata->url << std::endl;
+    std::cout << "Protocol: " << metadata->protocol << std::endl;
 
-    cout << "--------------- Headers Start ---------------------------------:" << endl;
-    map<string, string> headers = metadata->headers;
+    std::cout << "--------------- Headers Start ---------------------------------:" << std::endl;
+    std::map<std::string, std::string> headers = metadata->headers;
     for (auto i = headers.begin(); i != headers.end(); i++)
-        cout << i->first << "	 " << i->second << endl;
-    cout << "---------------  Headers End  ---------------------------------:" << endl;
+        std::cout << i->first << "	 " << i->second << std::endl;
+    std::cout << "---------------  Headers End  ---------------------------------:" << std::endl;
 
-    cout << "--------------- Body Content ---------------------------------:" << endl;
-    cout << metadata->body << endl;
+    std::cout << "--------------- Body Content ---------------------------------:" << std::endl;
+    std::cout << metadata->body << std::endl;
 }
